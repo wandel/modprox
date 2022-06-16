9faff228f430f1c8c54fd99fe6a30a5745e37cf5
wandel <16859422+wandel@users.noreply.github.com>
2022-06-17 03:50:09 +1000

    re-escapeing module paths before sending them to the golang module proxy

--- backend/proxy.go
+++ backend/proxy.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"golang.org/x/mod/module"
 	"io"
 	"log"
 	"net/http"
@@ -19,7 +20,12 @@ type ModuleProxy struct {
 
 func (b ModuleProxy) GetList(path, major string) ([]string, error) {
 	log.Println("list:", path, major)
-	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/list", path+major)
+	escaped, err := module.EscapePath(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to escape module path '%s'", path)
+	}
+
+	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/list", escaped+major)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to query module proxy")
@@ -43,7 +49,11 @@ func (b ModuleProxy) GetList(path, major string) ([]string, error) {
 
 func (b ModuleProxy) GetLatest(path, major string) (string, time.Time, error) {
 	log.Println("latest:", path, major)
-	url := fmt.Sprintf("https://proxy.golang.org/%s/@latest", path+major)
+	escaped, err := module.EscapePath(path)
+	if err != nil {
+		return "", time.Unix(0, 0), errors.Wrapf(err, "failed to escape module path '%s'", path)
+	}
+	url := fmt.Sprintf("https://proxy.golang.org/%s/@latest", escaped+major)
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", time.Unix(0, 0), errors.Wrap(err, "failed to query module proxy")
@@ -68,7 +78,11 @@ func (b ModuleProxy) GetLatest(path, major string) (string, time.Time, error) {
 
 func (b ModuleProxy) GetModule(path, version string) (string, error) {
 	log.Println("module:", path, version)
-	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.mod", path, version)
+	escaped, err := module.EscapePath(path)
+	if err != nil {
+		return "", errors.Wrapf(err, "failed to escape module path '%s'", path)
+	}
+	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.mod", escaped, version)
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to query module proxy")
@@ -89,7 +103,11 @@ func (b ModuleProxy) GetModule(path, version string) (string, error) {
 
 func (b ModuleProxy) GetInfo(path, version string) (string, time.Time, error) {
 	log.Println("info:", path, version)
-	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.info", path, version)
+	escaped, err := module.EscapePath(path)
+	if err != nil {
+		return "", time.Unix(0, 0), errors.Wrapf(err, "failed to escape module path '%s'", path)
+	}
+	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.info", escaped, version)
 	resp, err := http.Get(url)
 	if err != nil {
 		return "", time.Unix(0, 0), errors.Wrap(err, "failed to query module proxy")
@@ -114,7 +132,12 @@ func (b ModuleProxy) GetInfo(path, version string) (string, time.Time, error) {
 
 func (b ModuleProxy) GetArchive(path, version string) (io.Reader, error) {
 	log.Println("archive:", path, version)
-	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.zip", path, version)
+	escaped, err := module.EscapePath(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to escape module path '%s'", path)
+	}
+
+	url := fmt.Sprintf("https://proxy.golang.org/%s/@v/%s.zip", escaped, version)
 	resp, err := http.Get(url)
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to query module proxy")
